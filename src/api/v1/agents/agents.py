08e3289bdf718eed2f8fcd1a850e0b8bc781f759
Arvinth <arvinthram94@gmail.com>
2026-03-30 17:05:16 +0000

    ConversationBufferMemory Added

--- src/api/v1/agents/agents.py
+++ src/api/v1/agents/agents.py
@@ -1,8 +1,8 @@
 from langchain.agents import create_agent
 from langchain_core.tools import tool
-import os
 from dotenv import load_dotenv
 from src.api.v1.services.tools import vector_search_tool
+from langchain_classic.memory import ConversationBufferMemory
 
 load_dotenv(override = True)
 
@@ -36,14 +36,19 @@ my_agent = create_agent(
     system_prompt = "You are a RAG Bot who answers HR policy realted query . Please use the HR policy PDF using the given 'search_pdf_rag'.",
 )
 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
 def agents(query):
-    response = my_agent.invoke(
-        {
-            "messages":[
-                {"role":"user","content":f"{query}"}
-            ]
-        }
-    )
+
+    existing_history = memory.load_memory_variables({})["chat_history"]
+
+    messages = existing_history + [{"role": "user", "content": query}]
+
+    response = my_agent.invoke({"messages": messages})
+
+    response_text = response["messages"][-1].content
+
+    memory.save_context({"input": query}, {"output": response_text})
 
     print(response["messages"][-1].text)
     return response
